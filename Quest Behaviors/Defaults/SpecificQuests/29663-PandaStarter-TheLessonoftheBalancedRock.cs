863e6ce4e45925e295d582d4016988083aa1d9f7
chinajade247 <chinajade247@users.noreply.github.com>
2013-03-28 18:57:57 +0000

    BUGFIX: 29663-PandaStarter-TheLessonoftheBalancedRock.cs * Was pulling more than one Monk at a time due to dynamic target selection.  Fixed.  It now picks a target and kills it dead before selecting another. * The Pole counts as a vechicle, so many Combat Routines turn themselves off in this situation.  Now, we implement our own "Poor Man's Combat", and fall through to the main one. * MANY thanks go to Cava for finding and helping resolve this issue!

--- Quest Behaviors/Defaults/SpecificQuests/29663-PandaStarter-TheLessonoftheBalancedRock.cs
+++ Quest Behaviors/Defaults/SpecificQuests/29663-PandaStarter-TheLessonoftheBalancedRock.cs
@@ -72,19 +72,13 @@ namespace Honorbuddy.Quest_Behaviors.SpecificQuests.TheLessonoftheBalancedRock
         public QuestInLogRequirement QuestRequirementInLog { get; private set; }
         public WoWPoint Location { get; private set; }
 
+
         // Private variables for internal state
+        private LocalPlayer Me { get { return StyxWoW.Me; } }
+        private WoWUnit SelectedMonk { get; set; }
         private bool _isBehaviorDone;
         private bool _isDisposed;
         private Composite _root;
-
-
-
-        // Private properties
-        private LocalPlayer Me
-        {
-            get { return (StyxWoW.Me); }
-        }
-
 
 
         public void Dispose(bool isExplicitlyInitiatedDispose)
@@ -162,23 +156,18 @@ namespace Honorbuddy.Quest_Behaviors.SpecificQuests.TheLessonoftheBalancedRock
         }
 
 
-
-
-
         //<Vendor Name="Pearlfin Poolwatcher" Entry="55709" Type="Repair" X="-100.9809" Y="-2631.66" Z="2.150823" />
         //<Vendor Name="Pearlfin Poolwatcher" Entry="55711" Type="Repair" X="-130.8297" Y="-2636.422" Z="1.639656" />
 
         //209691 - sniper rifle
-        public WoWUnit Monk
+        public WoWUnit FindMonk()
         {
-            get
-            {
-                return
-                    ObjectManager.GetObjectsOfType<WoWUnit>().Where(r => (r.Entry == 55019 || r.Entry == 65468) && !r.IsFriendly).OrderBy(r=>r.Distance).FirstOrDefault();
-            }
+            return
+                ObjectManager.GetObjectsOfType<WoWUnit>()
+                .Where(r => (r.Entry == 55019 || r.Entry == 65468) && !r.IsFriendly)
+                .OrderBy(r=>r.Distance)
+                .FirstOrDefault();
         }
-
-
 
         
         public IEnumerable<WoWUnit> Poles
@@ -186,12 +175,11 @@ namespace Honorbuddy.Quest_Behaviors.SpecificQuests.TheLessonoftheBalancedRock
             get
             {
                 return
-                    ObjectManager.GetObjectsOfType<WoWUnit>().Where(r => (r.Entry == 54993 || r.Entry == 57626) && r.NpcFlags == 16777216);
+                    ObjectManager.GetObjectsOfType<WoWUnit>()
+                    .Where(r => (r.Entry == 54993 || r.Entry == 57626) && r.NpcFlags == 16777216);
             }
         }
 
-
-       
 
         private int stage = 0;
         WoWPoint spot = new WoWPoint(966.1218,3284.928,126.7932);
@@ -205,12 +193,32 @@ namespace Honorbuddy.Quest_Behaviors.SpecificQuests.TheLessonoftheBalancedRock
             get
             {
                 return new PrioritySelector(
-                    new Decorator(r=> !Me.InVehicle && Me.Location.Distance(spot) > 10, new Action(r=>Navigator.MoveTo(spot))),
-                    new Decorator(r=> !Me.InVehicle, new Action(r=>Poles.OrderBy(z=>z.Distance).FirstOrDefault().Interact(true)))
-                    
-                    
+                    new Decorator(r=> !Me.InVehicle && Me.Location.Distance(spot) > 10,
+                        new Action(r=>Navigator.MoveTo(spot))),
+                    new Decorator(r=> !Me.InVehicle,
+                        new Action(r=>Poles.OrderBy(z=>z.Distance).FirstOrDefault().Interact(true)))             
                     );
             }
+        }
+
+
+        // 24Feb2013-08:11UTC chinajade
+        private bool IsViable(WoWObject wowObject)
+        {
+            return
+                (wowObject != null)
+                && wowObject.IsValid;
+        }
+        
+        
+        // 24Feb2013-08:11UTC chinajade
+        private bool IsViableForFighting(WoWUnit wowUnit)
+        {
+            return
+                IsViable(wowUnit)
+                && wowUnit.IsAlive
+                && !wowUnit.IsFriendly
+                && !Blacklist.Contains(wowUnit, BlacklistFlags.Combat);
         }
 
 
@@ -219,49 +227,76 @@ namespace Honorbuddy.Quest_Behaviors.SpecificQuests.TheLessonoftheBalancedRock
             get
             {
                 return new PrioritySelector(
-                    new Decorator(r=> Monk.Distance > 5,
-                        new Action(delegate
-                        {
-                                                                            
-                            var Pole =
-                                Poles.Where(r => r.WithinInteractRange).
-                                    OrderBy(
-                                        r =>
-                                        r.Location.Distance(
-                                            Monk.Location)).FirstOrDefault();
-                            Pole.Interact(true);
-                        })),
+                    new Decorator(context => !IsViableForFighting(SelectedMonk),
+                        new Action(context => { SelectedMonk = FindMonk(); })),
 
-                    new Decorator(r=> Monk.Distance <= 5,
-                        new Sequence(        
-                            new Action(r=>{Monk.Target();Monk.Face(); return RunStatus.Success;}),
-                            RoutineManager.Current.CombatBehavior       
-                        ))
+                    new Decorator(r=> SelectedMonk.Distance <= 5,
+                        new PrioritySelector(
+                            new Decorator(context => Me.CurrentTarget != SelectedMonk,
+                                new Action(context => { SelectedMonk.Target(); })),
+
+                            new Decorator(context => !Me.IsSafelyFacing(SelectedMonk),
+                                new Action(context => { SelectedMonk.Face(); })),
+
+                            // Poor man's combat routine (in case main CR can't handle being in a vehicle)...
+                            new Action(context =>
+                            {
+                                SelectedMonk.Interact();  // "Auto-attack" at a minimum...
+                                return RunStatus.Failure; // fall through
+                            }),
+                            new Decorator(context => SpellManager.CanCast(3044),
+								new Action(context => { SpellManager.Cast(3044); })), // Hunter - Arcane Shot
+                            new Decorator(context => SpellManager.CanCast(56641),
+								new Action(context => { SpellManager.Cast(56641); })), // Hunter - Steady Shot
+                            new Decorator(context => SpellManager.CanCast(44614),
+                                new Action(context => { SpellManager.Cast(44614); })), // Mage   - Frostfire Bolt
+                            new Decorator(context => SpellManager.CanCast(2136),
+                                new Action(context => { SpellManager.Cast(2136); })), // Mage   - Fire Blast
+                            new Decorator(context => SpellManager.CanCast(100780),
+                                new Action(context => { SpellManager.Cast(100780); })), // Monk   - Jab
+                            new Decorator(context => SpellManager.CanCast(100787),
+                                new Action(context => { SpellManager.Cast(100787); })), // Monk   - Tiger Palm
+                            new Decorator(context => SpellManager.CanCast(585),
+                                new Action(context => { SpellManager.Cast(585); })), // Priest - Smite
+                            new Decorator(context => SpellManager.CanCast(1752),
+                                new Action(context => { SpellManager.Cast(1752); })), // Rogue  - Sinister Strike
+                            new Decorator(context => SpellManager.CanCast(2098),
+                                new Action(context => { SpellManager.Cast(2098); })), // Rogue  - Eviscerate
+                            new Decorator(context => SpellManager.CanCast(403),
+                                new Action(context => { SpellManager.Cast(403); })), // Shaman - Lightning Bolt
+                            new Decorator(context => SpellManager.CanCast(73899),
+                                new Action(context => { SpellManager.Cast(73899); })), // Shaman - Primal Strike
+                            new Decorator(context => SpellManager.CanCast(78),
+                                new Action(context => { SpellManager.Cast(78); })), // Warrior- Heroic Strike
+                            new Decorator(context => SpellManager.CanCast(34428),
+                                new Action(context => { SpellManager.Cast(34428); })), // Warrior- Victory Rush
+
+                            // Most combat routines will fail here, because being on the pole is considered being in a vehicle...
+                            new Decorator(targetContext => RoutineManager.Current.CombatBehavior != null,
+                                RoutineManager.Current.CombatBehavior),
+                            new Action(targetContext => { RoutineManager.Current.Combat(); })
+                        )),
+
+                    new Decorator(r=> SelectedMonk.Distance > 5,
+                        new Action(delegate
+                        {                                                                 
+                            var Pole =
+                                Poles.Where(r => r.WithinInteractRange)
+                                    .OrderBy(r => r.Location.Distance(SelectedMonk.Location))
+                                    .FirstOrDefault();
+                            Pole.Interact(true);
+                        }))
                 );
             }
         }
 
 
-        //if (SpellManager.CanCast("Blackout Kick"))
-        //{
-        //    SpellManager.Cast("Blackout Kick");
-        //}
-
-
-        //else if (SpellManager.CanCast("Tiger Palm") && !Me.HasAura("Tiger Power"))
-        //{
-        //    SpellManager.Cast("Tiger Palm");
-        //}
-
-        //else if (SpellManager.CanCast("Jab"))
-        //{
-        //    SpellManager.Cast("Jab");
-        //}
-
-
         protected override Composite CreateBehavior()
         {
-            return _root ?? (_root = new Decorator(ret => !_isBehaviorDone, new PrioritySelector(DoneYet, GetonPole,PoleCombat,new ActionAlwaysSucceed())));
+            return _root ?? (_root =
+                new Decorator(ret => !_isBehaviorDone,
+                    new PrioritySelector(DoneYet, GetonPole,PoleCombat, new ActionAlwaysSucceed()))
+            );
         }
 
 
