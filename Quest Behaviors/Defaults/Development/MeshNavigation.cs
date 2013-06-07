a3febb196deea747a2d354f8a4aae51ea5e06a65
chinajade247 <chinajade247@users.noreply.github.com>
2013-06-07 16:05:48 +0000

    REMOVED: Placeholder files that are no longer needed.

--- Quest Behaviors/Defaults/Development/MeshNavigation.cs
+++
@@ -1,7 +0,0 @@
-﻿// The previous contents of this file have been distributed to the QuestBehaviorCore
-// subdirectory tree to make them more managable.
-//
-// This comment file has been left in place to prevent 'symbol conflicts' for users that do
-// 'upgrade installs' of Honorbuddy.  The current Honorbuddy at the time of this writing is .557.
-// This file should be deleted as soon as it safe to do so (won't create problems for 'upgrade installers').
-//
--- Quest Behaviors/Defaults/QuestBehaviorCore/Extensions_WoWPoint.cs
+++
@@ -1,7 +0,0 @@
-﻿// The extensions contained in this file have been distributed to the QuestBehaviorCore/Extensions/
-// subdirectory to make them more managable.
-//
-// This comment file has been left in place to prevent 'symbol conflicts' for users that do
-// 'upgrade installs' of Honorbuddy.  The current Honorbuddy at the time of this writing is .557.
-// This file should be deleted as soon as it safe to do so (won't create problems for 'upgrade installers').
-//
