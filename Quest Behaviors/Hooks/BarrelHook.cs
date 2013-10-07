8968be02f7a8c61288f1ffb1038f2e82e2d8b67b
highvoltz <highvoltz@users.noreply.github.com>
2013-10-07 01:02:11 +0000

    BarrelHook improvements

--- Quest Behaviors/Hooks/BarrelHook.cs
+++ Quest Behaviors/Hooks/BarrelHook.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-
 using CommonBehaviors.Actions;
+using Honorbuddy.QuestBehaviorCore;
 using Styx;
 using Styx.Common;
 using Styx.CommonBot;
@@ -10,45 +10,56 @@ using Styx.Pathing;
 using Styx.TreeSharp;
 using Styx.WoWInternals;
 using Styx.WoWInternals.WoWObjects;
-
 using Action = Styx.TreeSharp.Action;
 
 
 namespace Honorbuddy.Quest_Behaviors.Hooks
 {
     [CustomBehaviorFileName(@"BarrelHook")]
-	[CustomBehaviorFileName(@"Hooks\BarrelHook")]
+    [CustomBehaviorFileName(@"Hooks\BarrelHook")]
     public class BarrelHook : CustomForcedBehavior
     {
-        public BarrelHook(Dictionary<string, string> args)
-            : base(args)
+        private static Composite _myHook;
+
+        public BarrelHook(Dictionary<string, string> args) : base(args)
         {
-            QuestId = 0;//GetAttributeAsQuestId("QuestId", true, null) ?? 0;
+            QuestId = 0; //GetAttributeAsQuestId("QuestId", true, null) ?? 0;
         }
+
         public int QuestId { get; set; }
-        
+
         public override bool IsDone
         {
-            get
-            {
-                return true;
-            }
+            get { return true; }
         }
 
-
-        private Composite myHook;
         public override void OnStart()
         {
-            OnStart_HandleAttributeProblem();
+            WoWUnit ookOok = null;
 
-            if (myHook == null)
+            OnStart_HandleAttributeProblem();
+            // 1st call will install hook and 2nd call will remove it.
+            if (_myHook == null)
             {
-                myHook = new Decorator(r => StyxWoW.Me.HasAura("Chuck Barrel"), new Action(r => Lua.DoString("RunMacroText('/click ExtraActionButton1')")));
-                TreeHooks.Instance.InsertHook("Combat_Main", 0, myHook);
+                 QBCLog.DeveloperInfo("Installing BarrelHook");
+                _myHook = new Decorator(
+                    r => StyxWoW.Me.HasAura("Chuck Barrel"),
+                    new PrioritySelector(
+                        ctx => ookOok = ObjectManager.GetObjectsOfTypeFast<WoWUnit>().FirstOrDefault(u => u.Entry == 57628),
+                        // only click the 'Break Barrel' button if ook is further than 20 units to - 
+                        // prevent getting barrel thrown at character again right after removing it.
+                        new Decorator(
+                            ctx => ookOok == null || ookOok.Distance > 20,
+                            new Action(r => Lua.DoString("RunMacroText('/click ExtraActionButton1')")))));
+
+
+                TreeHooks.Instance.InsertHook("Questbot_Main", 0, _myHook);
             }
             else
             {
-                TreeHooks.Instance.RemoveHook("Combat_Main",myHook);
+                QBCLog.DeveloperInfo("Removing BarrelHook");
+                TreeHooks.Instance.RemoveHook("Questbot_Main", _myHook);
+                _myHook = null;
             }
         }
     }
