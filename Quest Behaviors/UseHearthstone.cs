e4adc417340f807d29894f8b64930debb52a7dbb
chinajade247 <chinajade247@users.noreply.github.com>
2014-02-18 10:17:16 +0000

    HB-311

--- Quest Behaviors/UseHearthstone.cs
+++ Quest Behaviors/UseHearthstone.cs
@@ -10,19 +10,66 @@
 //
 
 #region Summary and Documentation
-// Allows you to use Transports.
-// ##Syntax##
-// TransportId: ID of the transport.
-// TransportStart: Start point of the transport that we will get on when its close enough to that point.
-// TransportEnd: End point of the transport that we will get off when its close enough to that point.
-// WaitAt: Where you wish to wait the transport at
-// GetOff: Where you wish to end up at when transport reaches TransportEnd point
-// StandOn: The point you wish the stand while you are in the transport
+//
+// QUICK DOX:
+// Uses the hearthstone, if available.
+// * Can optionally wait for the cooldown to complete.
+// * If you don't elect to wait for the cooldown, the profile will continue.
+//   In this situation, Honorbuddy will use whatever other transport options
+//   are available to it to move to the next destination.  (E.g., flying mount,
+//   ground mount, or in worse case 'on foot').
+//   ==> Honorbuddy cannot do inter-continental travel on its on.  If you planned
+//   ==> an inter-continental hearth, then you *must* wait for the cooldown to complete.
+//   ==> Or, you can write the profile to help Honorbuddy get to the other continent
+//   ==> if the hearthstone is on cooldown, and you don't want to wait.
+//
+// Basic Attributes:
+//      WaitForCD [optional;  Default: false]
+//          If true and the Hearthstone is on cooldown, Honorbuddy will wait for
+//          the Hearthstone cooldown to complete, then use it.
+//          If false and the Hearthstone is on cooldown, the behavior terminates
+//          immediately, and the profile continues.  If travel is on the same continent,
+//          Honorbuddy will provide the "plan B" travel.  However, if inter-continental
+//          travel is involved, the profile is expected to provide profile directives
+//          for "plan B" tranportation.
 //
 #endregion
 
 
 #region Examples
+// Toon is in Eastern Plaguelands, and hearth is set in Stormwind.
+// We choose to not wait for the cooldown.  If the hearthstone is on cooldown,
+// the toon fill find other means to get to Stormwind.  In the best case, this
+// will involve a Taxi ride.  In the worst case, this will involve walking.
+//    <CustomBehavior File="UseHearthstone" />
+//    <!-- continue with profile when hearthstone completes, or hearthstone on cooldown -->
+//
+// Toon is in Outland, and hearth is set in Stormwind.
+// We are lazy and don't want to write the required "Plan B" code for inter-continental
+// travel.  So we force a wait for the hearthstone cooldown, if the hearthstone is
+// not ready yet.
+//    <CustomBehavior File="UseHearthstone" WaitForCD="True" />
+//    <!-- profile continues once the hearth has been used -->
+//
+// Toon is in Outland, and hearth is set in Stormwind.
+// We choose not to wait for the hearth cooldown, if hearth is not available.
+// As such, we must provide the "Plan B" code for inter-continental travel,
+// if the hearth is not used.
+//    <CustomBehavior File="UseHearthstone" />
+//    <!-- "Plan B" for inter-continental travel, if hearth was on cooldown -->
+//    <If Condition="Me.MapId != 723">
+//        <CustomBehavior File="Message" Text="Flying or Shattrath to take portal to Stormwind" LogColor="Orange" />
+//        <CustomBehavior File="FlyTo" DestName="SW portal" X="-1956.413" Y="5383.551" Z="-12.42774" />
+//        <CustomBehavior File="ForcedDismount" />
+//        <MoveTo X="-1893.108" Y="5393.317" Z="-12.4277" />
+//        <!-- Use portal to Stormwind -->
+//        <CustomBehavior File="InteractWith" MobId="183325" X="-1893.108" Y="5393.317" Z="-12.4277" />
+//        <CustomBehavior File="WaitTimer" WaitTime="20000" GoalText="Waiting for zone {TimeRemaining}" />
+//    </If>
+//    <!-- profile continues once in Stormwind...
+//         We may have gotten here by hearth, or by moving through portal
+//      -->
+//
 #endregion
 
 
@@ -34,7 +81,6 @@ using System.Linq;
 using CommonBehaviors.Actions;
 using Honorbuddy.QuestBehaviorCore;
 using Styx;
-using Styx.Common;
 using Styx.CommonBot;
 using Styx.CommonBot.Profiles;
 using Styx.TreeSharp;
