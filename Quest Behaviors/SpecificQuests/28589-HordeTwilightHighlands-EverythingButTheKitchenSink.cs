60c6ea86024eae9f543e162f7283f4905090a450
highvoltz <highvoltz@users.noreply.github.com>
2014-09-10 03:21:59 +0000

    #HB-1111 Fixed ChangeLog 28589-HordeTwilightHighlands-EverythingButTheKitchenSink' quest behavior no-longer causes HB and WoW client to lockup

--- Quest Behaviors/SpecificQuests/28589-HordeTwilightHighlands-EverythingButTheKitchenSink.cs
+++ Quest Behaviors/SpecificQuests/28589-HordeTwilightHighlands-EverythingButTheKitchenSink.cs
@@ -21,7 +21,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using CommonBehaviors.Actions;
 using Honorbuddy.QuestBehaviorCore;
 using Styx;
 using Styx.Common;
@@ -39,14 +41,9 @@ using Action = Styx.TreeSharp.Action;
 namespace Honorbuddy.Quest_Behaviors.SpecificQuests.EverythingButTheKitchenSink
 {
 	[CustomBehaviorFileName(@"SpecificQuests\28589-HordeTwilightHighlands-EverythingButTheKitchenSink")]
-	public class KitchenSink : CustomForcedBehavior
+    public class KitchenSink : QuestBehaviorBase
 	{
-		~KitchenSink()
-		{
-			Dispose(false);
-		}
-
-		public KitchenSink(Dictionary<string, string> args)
+    	public KitchenSink(Dictionary<string, string> args)
 			: base(args)
 		{
 			QBCLog.BehaviorLoggingContext = this;
@@ -74,48 +71,11 @@ namespace Honorbuddy.Quest_Behaviors.SpecificQuests.EverythingButTheKitchenSink
 		}
 
 
-		// Attributes provided by caller
-		public int QuestId { get; private set; }
-		public QuestCompleteRequirement QuestRequirementComplete { get; private set; }
-		public QuestInLogRequirement QuestRequirementInLog { get; private set; }
-
 		// Private variables for internal state
-		private bool _isBehaviorDone;
-		private bool _isDisposed;
 		private Composite _root;
 
 
 		// Private properties
-		private LocalPlayer Me
-		{
-			get { return (StyxWoW.Me); }
-		}
-
-
-		public void Dispose(bool isExplicitlyInitiatedDispose)
-		{
-			if (!_isDisposed)
-			{
-				// NOTE: we should call any Dispose() method for any managed or unmanaged
-				// resource, if that resource provides a Dispose() method.
-
-				// Clean up managed resources, if explicit disposal...
-				if (isExplicitlyInitiatedDispose)
-				{
-					TreeHooks.Instance.RemoveHook("Questbot_Main", CreateBehavior_QuestbotMain());
-				}
-
-				// Clean up unmanaged resources (if any) here...
-				TreeRoot.GoalText = string.Empty;
-				TreeRoot.StatusText = string.Empty;
-
-				// Call parent Dispose() (if it exists) here ...
-				base.Dispose();
-			}
-
-			_isDisposed = true;
-		}
-
 
 		public Composite DoDps
 		{
@@ -129,146 +89,50 @@ namespace Honorbuddy.Quest_Behaviors.SpecificQuests.EverythingButTheKitchenSink
 			}
 		}
 
-
 		#region Overrides of CustomForcedBehavior
 
-		public WoWUnit Watcher
+        protected override void EvaluateUsage_DeprecatedAttributes(XElement xElement)
+        {
+        }
+
+        protected override void EvaluateUsage_SemanticCoherency(XElement xElement)
+        {
+        }
+
+
+		protected override Composite CreateBehavior_QuestbotMain()
 		{
-			get
-			{
-				return
-					ObjectManager.GetObjectsOfType<WoWUnit>().Where(u => u.Entry == 49683 && u.IsAlive).OrderBy(
-						u => u.Distance).FirstOrDefault();
-			}
+            return _root ?? (_root = new ActionRunCoroutine(ctx => MainCoroutine()));
 		}
 
-		public Composite TargetNew
-		{
-			get
-			{
-				return new Action(delegate
-				{
-					if (Watcher != null)
-						Watcher.Target();
-				});
-			}
-		}
+	    private async Task<bool> MainCoroutine()
+	    {
+	        if (IsDone)
+	            return false;
 
-		public Composite pewpew
-		{
-			get
-			{
-				return new Action(delegate
-				{
-					//WoWMovement.ClickToMove(Me.CurrentTarget.Location);
-					WoWMovement.ClickToMove(Me.CurrentTarget.Location.RayCast(Me.CurrentTarget.Rotation,20));
-var x= ObjectManager.GetObjectsOfType<WoWUnit>().FirstOrDefault(z => z.CharmedByUnit == Me);
+	        var ct = Me.CurrentTarget;
+	        if (ct == null || ct.DistanceSqr >= 100*100)
+	        {
+                var stormtalon = ObjectManager.GetObjectsOfType<WoWUnit>().Where(u => u.Entry == 49683 && u.IsAlive).OrderBy(
+                        u => u.DistanceSqr).FirstOrDefault();
+                if (stormtalon != null)
+                    stormtalon.Target();
+	            return true;
+	        }
 
-					Tripper.Tools.Math.Vector3 v = Me.CurrentTarget.Location - Me.Location;
-					v.Normalize();
-					Lua.DoString(
-						string.Format(
-							"VehicleAimIncrement({0} - VehicleAimGetAngle()); CastPetAction({1});",
-							Math.Asin(v.Z).ToString(), 1));
-
-				});
-			}
-		}
+            WoWMovement.ClickToMove(ct.Location.RayCast(ct.Rotation, 20));
+            var transport = (WoWUnit)StyxWoW.Me.Transport;
+            Tripper.Tools.Math.Vector3 v = ct.Location - transport.Location;
+            v.Normalize();
+	        var lua = string.Format(
+	            "VehicleAimIncrement(({0} - VehicleAimGetAngle())); CastPetAction({1});",
+	            Math.Asin(v.Z),
+	            1);
+            Lua.DoString(lua);
+	        return true;
+	    }
 
 
-
-
-
-		public Composite LockOn
-		{
-			get { return new Decorator(r => Me.CurrentTarget == null || Me.CurrentTarget.Distance > 100, TargetNew); }
-		}
-
-
-		private WoWPoint endspot = new WoWPoint(1076.7, 455.7638, -44.20478);
-		private WoWPoint spot = new WoWPoint(1109.848, 462.9017, -45.03053);
-
-
-
-
-		protected Composite CreateBehavior_QuestbotMain()
-		{
-			//return _root ?? (_root = new Decorator(ret => !_isBehaviorDone, new PrioritySelector(ShootArrows,Lazor, BunchUp, new ActionAlwaysSucceed())));
-			return _root ?? (_root = new Decorator(ret => !_isBehaviorDone, new PrioritySelector(new Action(ret => Loopstuff()))));
-		}
-
-
-
-		public void Loopstuff()
-		{
-			while (true)
-			{
-				ObjectManager.Update();
-				if (Me.IsQuestComplete(QuestId))
-				{
-					_isBehaviorDone = true;
-					break;
-				}
-
-				if (Me.CurrentTarget != null && Me.CurrentTarget.Distance < 100)
-				{
-					//WoWMovement.ClickToMove(Me.CurrentTarget.Location);
-					WoWMovement.ClickToMove(Me.CurrentTarget.Location.RayCast(Me.CurrentTarget.Rotation, 20));
-					var x =
-						ObjectManager.GetObjectsOfType<WoWUnit>().FirstOrDefault(z => z.CharmedByUnit == Me);
-
-					Tripper.Tools.Math.Vector3 v = Me.CurrentTarget.Location - x.Location;
-					v.Normalize();
-					Lua.DoString(
-						string.Format(
-							"VehicleAimIncrement(({0} - VehicleAimGetAngle())); CastPetAction({1});",
-							Math.Asin(v.Z).ToString(), 1));
-				}
-				else
-				{
-					if (Watcher != null)
-						Watcher.Target();
-				}
-
-
-			}
-		}
-
-
-		public override void Dispose()
-		{
-			Dispose(true);
-			GC.SuppressFinalize(this);
-		}
-
-
-		public override bool IsDone
-		{
-			get
-			{
-				return (_isBehaviorDone // normal completion
-						|| !UtilIsProgressRequirementsMet(QuestId, QuestRequirementInLog, QuestRequirementComplete));
-			}
-		}
-
-
-		public override void OnStart()
-		{
-			// This reports problems, and stops BT processing if there was a problem with attributes...
-			// We had to defer this action, as the 'profile line number' is not available during the element's
-			// constructor call.
-			OnStart_HandleAttributeProblem();
-
-			// If the quest is complete, this behavior is already done...
-			// So we don't want to falsely inform the user of things that will be skipped.
-			if (!IsDone)
-			{
-				TreeHooks.Instance.InsertHook("Questbot_Main", 0, CreateBehavior_QuestbotMain());
-
-				this.UpdateGoalText(QuestId);
-			}
-		}
-
-		#endregion
+	    #endregion
 	}
 }
