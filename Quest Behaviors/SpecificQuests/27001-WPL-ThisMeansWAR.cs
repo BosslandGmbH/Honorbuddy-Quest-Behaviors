b271156d1729f93a04dba0635fd19e0bb4fbf8d1
highvoltz <highvoltz@users.noreply.github.com>
2015-01-01 17:15:38 +0000

    ThisMeansWAR Quest specific QB no-longer spams the whinny button

--- Quest Behaviors/SpecificQuests/27001-WPL-ThisMeansWAR.cs
+++ Quest Behaviors/SpecificQuests/27001-WPL-ThisMeansWAR.cs
@@ -22,13 +22,17 @@
 #region Usings
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-
+using System.Threading.Tasks;
+using Buddy.Coroutines;
 using CommonBehaviors.Actions;
 using Honorbuddy.QuestBehaviorCore;
+using Honorbuddy.Quest_Behaviors.WaitTimerBehavior;
 using Styx;
 using Styx.Common;
 using Styx.CommonBot;
+using Styx.CommonBot.Coroutines;
 using Styx.CommonBot.Profiles;
 using Styx.Helpers;
 using Styx.Pathing;
@@ -54,13 +58,10 @@ namespace Honorbuddy.Quest_Behaviors.SpecificQuests.ThisMeansWAR
 
 		private LocalPlayer Me { get { return (StyxWoW.Me); } }
 		readonly WoWPoint _lumberMillLocation = new WoWPoint(2427.133, -1649.115, 104.0841);
-		readonly WoWPoint _spiderLocation = new WoWPoint(2332.387, -1694.623, 104.5099);
-		private WoWPoint _movetoPoint;
+		readonly WoWPoint _spiderSpawnLocation = new WoWPoint(2332.387, -1694.623, 104.5099);
+		private  WoWPoint _spiderScareLoc;
 		private WoWUnit _currentTarget;
-
-        readonly List<WoWGuid> _blackList = new List<WoWGuid>();
-		private DateTime _stuckTimeStamp = DateTime.Now;
-		private WoWPoint _lastMovetoPoint;
+		Stopwatch _blacklistTimer = new Stopwatch();
 
 		public override bool IsDone
 		{
@@ -99,93 +100,75 @@ namespace Honorbuddy.Quest_Behaviors.SpecificQuests.ThisMeansWAR
 		private Composite _root;
 		protected override Composite CreateBehavior()
 		{
-			return _root ?? (_root = new PrioritySelector(
-				// if not in a turret than move to one and interact with it
-				new Decorator(ret => !Query.IsInVehicle(),
-					new Sequence(ctx => GetMustang(), // set Turret as context
-						new DecoratorContinue(ctx => ctx != null && ((WoWUnit)ctx).DistanceSqr > 5 * 5,
-							new Action(ctx =>
-										   {
-											   Navigator.MoveTo(((WoWUnit)ctx).Location);
-											   TreeRoot.StatusText = "Moving To Mustang";
-										   })),
-						new DecoratorContinue(ctx => ctx != null && ((WoWUnit)ctx).DistanceSqr <= 5 * 5,
-							new Sequence(
-								new Mount.ActionLandAndDismount(),
-								new Action(ctx =>
-								{
-									QBCLog.Info("Interacting with Mustang");
-									((WoWUnit)ctx).Interact();
-								}))))),
-				// Find the nearest spider and if none exist then move to thier spawn location
-					new Decorator(ret => _currentTarget == null || !_currentTarget.IsValid || !_currentTarget.IsAlive,
-							new Action(ctx =>
-										   {
-											   _currentTarget = ObjectManager.GetObjectsOfType<WoWUnit>()
-												   .Where(
-													   u =>
-													   u.IsAlive && !_blackList.Contains(u.Guid) && u.Entry == 44284).
-												   OrderBy(u => u.DistanceSqr).FirstOrDefault();
-											   if (_currentTarget == null)
-											   {
-												   Navigator.MoveTo(_spiderLocation);
-												   QBCLog.Info("No spiders found. Moving to spawn point");
-											   }
-											   else
-											   {
-												   _movetoPoint = WoWMathHelper.CalculatePointFrom(_lumberMillLocation,
-																								   _currentTarget.
-																									   Location, -5);
-												   QBCLog.Info("Locked on a new target. Distance {0}", _currentTarget.Distance);
-											   }
-										   })),
+			return _root ?? (_root = new ActionRunCoroutine((ctx => ScareSpiders())));
+		}
 
+		private async Task<bool> ScareSpiders()
+		{
+			// if not in a turret than move to one and interact with it
+			if (!Query.IsInVehicle())
+			{
+				var mustang = GetMustang();
+				if (mustang == null)
+				{
+					QBCLog.Warning("No mustang was found nearby");
+					return false;
+				}
+				
+				TreeRoot.StatusText = "Moving To Mustang";
+				if (mustang.DistanceSqr > 5*5)
+					return (await CommonCoroutines.MoveTo(mustang.Location)).IsSuccessful();
 
-							new Sequence(
-								new Action(ctx => TreeRoot.StatusText = "Scaring spider towards lumber mill"),
-								new Action(ctx =>
-											   { // blacklist spider if it doesn't move
-												   if (DateTime.Now - _stuckTimeStamp > TimeSpan.FromSeconds(6))
-												   {
-													   _stuckTimeStamp = DateTime.Now;
-													   if (_movetoPoint.DistanceSqr(_lastMovetoPoint) < 3 * 3)
-													   {
-														   QBCLog.Info("Blacklisting spider");
-														   _blackList.Add(_currentTarget.Guid);
-														   _currentTarget = null;
-														   return RunStatus.Failure;
-													   }
-													   _lastMovetoPoint = _movetoPoint;
-												   }
-												   return RunStatus.Success;
-											   }),
-								new Action(ctx =>
-												{
-													// update movepoint
-													_movetoPoint =
-														WoWMathHelper.CalculatePointFrom(_lumberMillLocation,
-																						_currentTarget.
-																							Location, -6);
-													if (_movetoPoint.DistanceSqr(Me.Location) >4 * 4)
-													{
-														Navigator.MoveTo(_movetoPoint);
-														return RunStatus.Running;
-													}
-													return RunStatus.Success;
-												}),
-								new WaitContinue(2, ret => !Me.IsMoving, new ActionAlwaysSucceed()),
-								new Action(ctx =>
-											   {
-												   
-													   Me.SetFacing(_lumberMillLocation);
-													   WoWMovement.Move(WoWMovement.MovementDirection.ForwardBackMovement);
-													   WoWMovement.MoveStop(WoWMovement.MovementDirection.ForwardBackMovement);
-													   //Lua.DoString("CastSpellByID(83605)");
-												   
-											   }),
-							new WaitContinue(TimeSpan.FromMilliseconds(200), ret => false, new ActionAlwaysSucceed()),
-							 new Action(ctx => Lua.DoString("CastSpellByID(83605)"))
-								 )));
+				await CommonCoroutines.LandAndDismount();
+				QBCLog.Info("Interacting with Mustang");
+				mustang.Interact();
+				return true;
+			}
+
+			// Find the nearest spider and if none exist then move to the spawn location
+			if (!Query.IsViable(_currentTarget) || !_currentTarget.IsAlive)
+			{
+				_currentTarget = ObjectManager.GetObjectsOfType<WoWUnit>()
+					.Where(u => u.IsAlive && u.Entry == 44284 && !Blacklist.Contains(u, BlacklistFlags.Interact))
+					.OrderBy(u => u.DistanceSqr).FirstOrDefault();
+
+				if (_currentTarget == null)
+				{
+					if (!Navigator.AtLocation(_spiderSpawnLocation))
+						return (await CommonCoroutines.MoveTo(_spiderSpawnLocation)).IsSuccessful();
+					TreeRoot.StatusText = "Waiting for spiders to spawn";
+					return true;
+				}
+
+				QBCLog.Info("Locked on a new target. Distance {0}", _currentTarget.Distance);
+			}
+
+			TreeRoot.StatusText = "Scaring spider towards lumber mill";
+
+			var moveToPoint = WoWMathHelper.CalculatePointFrom(_lumberMillLocation, _currentTarget.Location, -6);
+
+			if (moveToPoint.DistanceSqr((WoWMovement.ActiveMover ?? StyxWoW.Me).Location) > 4 * 4)
+				return (await CommonCoroutines.MoveTo(moveToPoint)).IsSuccessful();
+
+			// spider not moving? blacklist and find a new target.
+			if (_blacklistTimer.ElapsedMilliseconds > 5000 && _currentTarget.Location.DistanceSqr(_spiderScareLoc) < 1*1)
+			{
+				Blacklist.Add(_currentTarget, BlacklistFlags.Interact, TimeSpan.FromSeconds(30));
+				_currentTarget = null;
+			}
+			else if (!_currentTarget.HasAura("Fear"))
+			{
+				await CommonCoroutines.StopMoving();
+				Me.SetFacing(_lumberMillLocation);
+				await CommonCoroutines.SleepForLagDuration();
+				await Coroutine.Sleep(200);
+				_spiderScareLoc = _currentTarget.Location;
+				_blacklistTimer.Restart();
+				Lua.DoString("CastSpellByID(83605)");
+				await Coroutine.Wait(3000, () => Query.IsViable(_currentTarget) && _currentTarget.HasAura("Fear"));
+			}
+
+			return true;
 		}
 
 
